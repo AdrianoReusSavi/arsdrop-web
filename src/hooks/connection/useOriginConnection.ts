5865a013e2f45f2405ecae9ee529b076f1fe5a93
Adriano <91353735+AdrianoReusSavi@users.noreply.github.com>
2025-05-15 10:08:06 -0300

    Atualizando origem para ter redundância

--- src/hooks/connection/useOriginConnection.ts
+++ src/hooks/connection/useOriginConnection.ts
@@ -35,7 +35,11 @@ export default function useOriginConnection(token: string | null) {
           iceServers: [
             { urls: 'stun:stun.l.google.com:19302' },
             {
-              urls: 'turn:openrelay.metered.ca:80',
+              urls: [
+                'turn:openrelay.metered.ca:80',
+                'turn:openrelay.metered.ca:443',
+                'turn:openrelay.metered.ca:443?transport=tcp'
+              ],
               username: 'openrelayproject',
               credential: 'openrelayproject'
             }
